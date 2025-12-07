dc2286c604ba3568f4fd862417e07850ec64beec
AlxBDo <alexbidaud85@gmail.com>
2025-12-07 15:34:16 +0100

    add isEmpty utils

--- src/lib/main.ts
+++ src/lib/main.ts
@@ -4,5 +4,6 @@ import Store from '../core/Store'
 export { createPlugin } from "../plugins/createPlugin"
 export { defineAStore } from "../utils/store"
 export { eppsLog, eppsLogError } from "../utils/log"
+export { isEmpty } from "../utils/validation"
 export { PluginCreation }
 export { Store }
